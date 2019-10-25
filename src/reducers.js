5829bcada13ec54c54869560746b150cec4b4479
wangxuhao <wangxuhao@sensetime.com>
2019-10-25 17:09:43 +0800

    Modify UI

--- src/reducers.js
+++ src/reducers.js
@@ -3,6 +3,7 @@ import { routerReducer } from 'react-router-redux';
 import home from 'containers/Home/reducer';
 import tagPost from 'containers/TagPost/reducer';
 import article from 'containers/Article/reducer';
+import dashboard from 'containers/Dashboard/reducer';
 
 export default asyncReducers =>
   combineReducers({
